44b53e42c47da72549e0b689867343aff91ace1c
shawn-778 <xsy@stu.pku.edu.cn>
2025-04-08 20:57:12 +0800

    Update difference.py

--- difference.py
+++ difference.py
@@ -62,3 +62,47 @@ plt.subplot(2, 2, 4)
 plt.plot(indices, e_xx_2)
 plt.xticks(indices, [f'{d:.{max_index - 1}f}' for d in delta_x], rotation=30)
 plt.show()
+# 第三题
+print("-" * 200)
+print('question 3')
+max_index = 5
+max_polynomial_index = 3
+indices = list(range(max_index))  # 用于检验格式精度的指数
+polynomial_indices = list(range(max_polynomial_index))  # 当项数为3时没有截断误差
+parameters = [random.uniform(-10, 10) for _ in range(max_polynomial_index)]  # 随机生成多项式系数
+f = sum(parameter * x ** index for parameter in parameters for index in polynomial_indices)
+g = sp.sin(x)
+
+
+def solve_error(f):  # 求误差
+    f_x = sp.diff(f, x, 1)
+    f_xx = sp.diff(f, x, 2)
+    delta_x = [0.1 ** index for index in indices]  # 不断减小的delta_x
+    x_0 = random.uniform(-1, 1)
+    u_1 = [diff_1(f, x_0, delta_x[index]) for index in indices]
+    u_x_1, u_xx_1 = zip(*u_1)
+    u_2 = [diff_2(f, x_0, delta_x[index]) for index in indices]
+    u_x_2, u_xx_2 = zip(*u_2)
+    u_x_s = f_x.subs(x, x_0)
+    u_xx_s = f_xx.subs(x, x_0)
+    e_x_1 = [-u_x_s + u_x_1[index] for index in indices]
+    e_xx_1 = [-u_xx_s + u_xx_1[index] for index in indices]
+    e_x_2 = [-u_x_s + u_x_2[index] for index in indices]
+    e_xx_2 = [-u_xx_s + u_xx_2[index] for index in indices]
+    return e_x_1, e_x_2, e_xx_1, e_xx_2
+
+
+e_x_1, e_x_2, e_xx_1, e_xx_2 = solve_error(f)
+print("舍入误差：")
+print(e_x_1)
+print(e_x_2)
+print(e_xx_1)
+print(e_xx_2)
+e_x_1, e_x_2, e_xx_1, e_xx_2 = solve_error(g)
+print("截断误差和舍入误差")
+print(e_x_1)
+print(e_x_2)
+print(e_xx_1)
+print(e_xx_2)
+
+print("-" * 200)
